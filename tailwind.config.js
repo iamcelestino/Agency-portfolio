3f6fcc81b8e11e1fe6ee4720a72a85b6ae974fd5
iamcelestino <trcelestino488@gmail.com>
2024-04-25 10:53:26 -0700

    feat: add navbar, and banner

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    content: [
+        "./public/**/*.html",
+        "./src/**/*.js"
+    ],
+    theme: {
+      extend: {},
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          sm: '1.5rem',
+          md: '3rem',
+          xl: '5rem'       
+        }
+      }
+    },
+    plugins: [],
+  }
+  
